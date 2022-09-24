16379bb5c40e2100f51e6cad7b2c961289450e1b
sanjanamadhu <sanjana.s.madhu@gmail.com>
2022-09-24 15:16:20 -0400

    added code to print families

--- Assignment2_GEDCOM.py
+++ Assignment2_GEDCOM.py
@@ -173,28 +173,65 @@ for line in file:
                 temp.append(False)
                 modified_file.append(temp)
 
-print(modified_file)
+j = 1
+for i in range(len(modified_file)):  
+    if (modified_file[i][0] == '0' and modified_file[i][1] == "FAM"):
+        j = j + 1     
+    if (modified_file[i][0] == '0' and modified_file[i][1] == "INDI"):
+        j = j + 1 
+    modified_file[i].append(j)
 
+j = 1
+clusters_list = []
+cluster = []
+for i in range(len(modified_file)):   
+    if modified_file[i][-1] == j:
+        cluster.append(modified_file[i])
+    else:
+        clusters_list.append(cluster)
+        cluster = []
+        cluster.append(modified_file[i])
+        j = j + 1
 
-
-
+for i in range(len(clusters_list)):
+    if (clusters_list[i][0][1] == "FAM"):
+        children = set()
+        married = "N/A"
+        divorced = "N/A"
+        wife_id = "N/A"
+        husband_id = "N/A"
+        husband_name = "N/A"
+        wife_name = "N/A"
+        id = clusters_list[i][0][2]
+        for j in range(len(clusters_list[i])):
+            if (clusters_list[i][j][1] == "MARR"):
+                married = clusters_list[i][j+1][2]
+            if (clusters_list[i][j][1] == "DIV"):
+                divorced = clusters_list[i][j+1][2]
+            if(clusters_list[i][j][1] == "HUSB"):
+                husband_id = clusters_list[i][j][2]
+            if(clusters_list[i][j][1] == "WIFE"):
+                wife_id = clusters_list[i][j][2]
+            if(clusters_list[i][j][1] == "CHIL"):
+                children.add(clusters_list[i][j][2])
+        if (len(children) == 0):
+            children = {}
+        wife_name = ""
+        husband_name = ""
+        for i in range(len(modified_file)):
+            if modified_file[i][0] == "0" and  modified_file[i][1] == "INDI" and modified_file[i][2] == husband_id:
+                if (modified_file[i+1][1] == "NAME"):
+                    husband_name = modified_file[i+1][2]
+            if modified_file[i][0] == "0" and  modified_file[i][1] == "INDI" and modified_file[i][2] == wife_id:
+                if (modified_file[i+1][1] == "NAME"):
+                    wife_name = modified_file[i+1][2]
+        families.add_row([id, married, divorced, husband_id, husband_name, wife_id, wife_name, children])
         
 
-'''
-for line in file:
+    # if (clusters_list[i][0][1] == "INDI"):
 
-    words = line.split()
 
-    if(len(words) == 0):
-        continue
-
-    if (words[0] == '0'):
-
-        if (words[2] == "FAM"):    
-            print("do something")
-        elif (words[2] == "INDI"):
-            print("do something")
-
- '''
+print(families)
+    
 
             
