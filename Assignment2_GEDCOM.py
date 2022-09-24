1dfc341847f19d184c9e5992c87d1e4b83e10ed8
lkjosyula <56276641+lkjosyula@users.noreply.github.com>
2022-09-24 00:08:57 -0400

    changes to create a list that we can easily iterate through

--- Assignment2_GEDCOM.py
+++ Assignment2_GEDCOM.py
@@ -12,6 +12,175 @@ acceptable_tags_0 = ['INDI', 'FAM', 'HEAD', 'TRLR', 'NOTE']
 acceptable_tags_1 = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV']
 acceptable_tags_2 = ['DATE']
 
+
+#Code to essentially take the entire input file, and make it into a list of lists. Each list
+#is each line. Each list's zero index is the level. First index is tag. Second index is the 
+#notes/extra information. Third index is a boolean indicating if this line was level 0.
+
+modified_file = []
+for line in file:
+    words = line.split()
+
+    if (words[0] == '0'):
+
+        sawZero = 0
+        if (words[1] in acceptable_tags_0):
+            if (words[1] == 'HEAD' or words[1] == 'TRLR' or words[1] == 'NOTE'):
+                temp = []
+                level = words[0]
+                tag = words[1]
+                words.pop(0)
+                words.pop(0)
+                if (len(words) == 0):
+                    temp.append(level)
+                    temp.append(tag)
+                    temp.append(' ')
+                    temp.append(True)
+                    modified_file.append(temp)
+                else:
+                    sent = " ".join(words)
+                    temp.append(level)
+                    temp.append(tag)
+                    temp.append(sent)
+                    temp.append(True)
+                    #print(temp)
+                    modified_file.append(temp)
+
+        elif (words[2] in acceptable_tags_0):
+            if (words[2] == 'INDI' or words[2] == 'FAM'):
+                temp = []
+                temp.append(words[0].strip())
+                temp.append(words[2].strip())
+                temp.append(words[1].strip())
+                temp.append(True)
+                modified_file.append(temp)
+            else:
+                temp = []
+                level = words[0]
+                tag = words[2]
+                words.pop(0)
+                words.pop(1)
+                if (len(words) == 0):
+                    temp.append(level)
+                    temp.append(tag)
+                    temp.append(' ')
+                    temp.append(True)
+                    modified_file.append(temp)
+                else:
+                    sent = " ".join(words)
+                    temp.append(level)
+                    temp.append(tag)
+                    temp.append(sent)
+                    temp.append(True)
+                    modified_file.append(temp)
+        else:
+            temp = []
+            level = words[0]
+            tag = words[1]
+            words.pop(0)
+            words.pop(0)
+            if (len(words) == 0):
+                temp.append(level)
+                temp.append(tag)
+                temp.append(' ')
+                temp.append(True)
+                modified_file.append(temp)            
+            else:
+                sent = " ".join(words)
+                temp.append(level)
+                temp.append(tag)
+                temp.append(sent)
+                temp.append(True)
+                modified_file.append(temp)
+    
+    elif (words[0] == '1'):
+        if (words[1] in acceptable_tags_1):
+            temp = []
+            level = words[0]
+            tag = words[1]
+            words.pop(0)
+            words.pop(0)
+            if(len(words) == 0):
+                temp.append(level)
+                temp.append(tag)
+                temp.append(' ')
+                temp.append(False)
+                modified_file.append(temp)
+            else:
+                sent = " ".join(words)
+                temp.append(level)
+                temp.append(tag)
+                temp.append(sent)
+                temp.append(False)
+                modified_file.append(temp)
+        else:
+            temp = []
+            level = words[0]
+            tag = words[1]
+            words.pop(0)
+            words.pop(0)
+            if(len(words) == 0):
+                temp.append(level)
+                temp.append(tag)
+                temp.append(' ')
+                temp.append(False)
+                modified_file.append(temp)
+            else:
+                sent = " ".join(words)
+                temp.append(level)
+                temp.append(tag)
+                temp.append(sent)
+                temp.append(False)
+                modified_file.append(temp)
+
+    elif (words[0] == '2'):
+        if (words[1] in acceptable_tags_2):
+            temp = []
+            level = words[0]
+            tag = words[1]
+            words.pop(0)
+            words.pop(0)
+            if(len(words) == 0):
+                temp.append(level)
+                temp.append(tag)
+                temp.append(' ')
+                temp.append(False)
+                modified_file.append(temp)
+            else:
+                sent = " ".join(words)
+                temp.append(level)
+                temp.append(tag)
+                temp.append(sent)
+                temp.append(False)
+                modified_file.append(temp)
+        else:
+            temp = []
+            level = words[0]
+            tag = words[1]
+            words.pop(0)
+            words.pop(0)
+            if(len(words) == 0):
+                temp.append(level)
+                temp.append(tag)
+                temp.append(' ')
+                temp.append(False)
+                modified_file.append(temp)
+            else:
+                sent = " ".join(words)
+                temp.append(level)
+                temp.append(tag)
+                temp.append(sent)
+                temp.append(False)
+                modified_file.append(temp)
+
+print(modified_file)
+
+
+
+
+        
+
+'''
 for line in file:
 
     words = line.split()
@@ -26,6 +195,6 @@ for line in file:
         elif (words[2] == "INDI"):
             print("do something")
 
- 
+ '''
 
             
