871e8969c6fdcce61fc36eca8547526d1be597eb
DanielleDauphinais <72697849+DanielleDauphinais@users.noreply.github.com>
2022-09-28 22:08:03 -0400

    Add files via upload
    Has Marriage before divorce function needs to be tested more and clusters_list has duplicate families.

--- CS-555-main/Assignment2_GEDCOM.py
+++ CS-555-main/Assignment2_GEDCOM.py
@@ -375,13 +375,41 @@ Story Name: Birth before death
 Description: Birth should occur before death of an individual
 '''
 def birthBeforeDeath():
-    for i in range(len(indi_list)):
-        if (indi_list[i][4] != "N/A"):
-            if (indi_list[i][4] <= 0):
-                print("Error USO3: Birth data of " + indi_list[i][1]+ " ")
+    for i in range(len(final_indi)):
+        if (final_indi[i][4] != "N/A"):
+            if (final_indi[i][4] <= 0):
+                print("Error USO3: Birth data of " + final_indi[i][1]+ "(" + final_indi[i][2] +")" "occurs after his death date.")
 
+'''
+US04 - Sprint 1
+Story Name: Marriage before divorce
+Description: Marriage should occur before divorce of spouses, and divorce can only occur after marriage
+'''
+def marrigeBeforeDivorce():
+    for i in range(len(clusters_list)):
+        if (clusters_list[i][0][1] == "FAM"):
+            id = clusters_list[i][0][2]
+            MARR = 0
+            DIV = 0
+            for j in range(len(clusters_list[i])):
+                if (clusters_list[i][j][1] == "MARR"):
+                    MARR = datetime.strptime(clusters_list[i][j+1][2], '%d %b %Y').date()
+                if (clusters_list[i][j][1] == "DIV"):
+                    DIV = datetime.strptime(clusters_list[i][j+1][2], '%d %b %Y').date()
+                if (type(MARR) ==  type(DIV)):
+                    if(MARR != 0):
+                        if (DIV < MARR):
+                            print("Error US04: In family " + id + " Divorce occurs before marrage.")
+                if (MARR == 0 and DIV != 0):
+                    print("Error US04: In family " + id + " divorce but no marrage.")
+            
+
+
+
+            
 
 print(families)
-birthBeforeDeath()
+
+
     
             
