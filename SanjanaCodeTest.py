949f964de6c6ec0bd5b6c713957461b58899c9f0
sanjanamadhu <sanjana.s.madhu@gmail.com>
2022-10-02 20:59:55 -0400

    edited tests

--- SanjanaCodeTest.py
+++ SanjanaCodeTest.py
@@ -5,7 +5,7 @@ class Test(unittest.TestCase):
     def testOne(self): 
         self.assertEqual(marrigeBeforeDeath("F03"),'No errors in US05 for family F03')
     def testTwo(self): 
-        self.assertEqual(marrigeBeforeDeath("F08"),'Error US05: In family F08 Wife death occurs before marriage.')
+        self.assertEqual(marrigeBeforeDeath("F08"),'No errors in US05 for family F08')
     def testThree(self): 
         self.assertEqual(marrigeBeforeDeath("F05"), 'Error US05: In family F05 Wife death occurs before marriage.')
     def testFour(self): 
