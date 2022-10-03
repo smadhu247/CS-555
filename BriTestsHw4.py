7f422df1585ee7ae012296f7ed43a0620768dec6
Brianna Garland <56358388+BriannaPGarland@users.noreply.github.com>
2022-10-03 01:13:26 -0400

    Update BriTestsHw4.py

--- BriTestsHw4.py
+++ BriTestsHw4.py
@@ -6,12 +6,12 @@ class Test(unittest.TestCase):
     def test_deathLessThan150_1(self): 
         self.assertEqual(deathLessThan150("I01"),'Error US07: With Individual:  I01, Nancy /Below/, Individual is listed as over 150 years old & Death must be within 150 years of birth')
     def test_deathLessThan150_2(self): 
-        self.assertEqual(childDuringMarriage("F03"),'No Errors with US07')
+        self.assertEqual(deathLessThan150("I03"),'No Errors with US07')
     def test_childDuringMarriage_1(self): 
-        self.assertEqual(deathLessThan150("F03"),'Error US08: Child born out of side of parents marriage Timeline for I03, David /Dauphinais/')
+        self.assertEqual(childDuringMarriage("F03"),'Error US08: Child born out of side of parents marriage Timeline for I03, David /Dauphinais/')
     def test_childDuringMarriage_2(self): 
-        self.assertEqual(deathLessThan150("F05"),'Error US08: Child born out of side of parents marriage Timeline for I06, Peter /Smith/')
+        self.assertEqual(childDuringMarriage("F05"),'Error US08: Birthday = N/A for I06 Peter /Smith/')
     def test_childDuringMarriage_3(self): 
-        self.assertEqual(deathLessThan150("F06"),'Error US08: Child born out of side of parents marriage Timeline for I08, Danielle /Dauphinais/')
+        self.assertEqual(childDuringMarriage('F06'),'Error US08: Child born out of side of parents marriage Timeline for I08, Danielle /Dauphinais/')
 if __name__ == '__main__':
     unittest.main()
